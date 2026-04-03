e92fb2c374b18cc9ba9b3b9914107c122f167af6
Sedian Paul P. Reyes <50647987+BalongReyes@users.noreply.github.com>
2026-04-03 23:15:16 +0800

    Version 0.3.21

--- app/src/main/java/DatabaseSystem/DataTable/DataTableFilter.java
+++ app/src/main/java/DatabaseSystem/DataTable/DataTableFilter.java
@@ -4,6 +4,7 @@ package DatabaseSystem.DataTable;
 public class DataTableFilter{
 
     private int dataIndex;
+    private String dataWhere;
     private DataTableOrder order;
 
     public DataTableFilter(int dataIndex, DataTableOrder order){
@@ -11,8 +12,18 @@ public class DataTableFilter{
         this.order = order;
     }
 
+    public DataTableFilter(int dataIndex, DataTableOrder order, String dataWhere){
+        this.dataIndex = dataIndex;
+        this.order = order;
+        this.dataWhere = dataWhere;
+    }
+
     public int getDataIndex(){
         return dataIndex;
+    }
+
+    public String getDataWhere(){
+        return dataWhere;
     }
 
     public DataTableOrder getOrder(){
